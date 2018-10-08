b4f4eb319480cc60df990150e25bfe110569b957
Julien Delbez <julien.delbez@hotmail.fr>
2018-10-08 21:30:22 +0200

    ajout de la quete 2

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -1,8 +1,19 @@
 <?php
-// src/Controller/ItemController.php
-require __DIR__ . '/../Model/ItemManager.php';
+namespace Controller;
 
-$items = selectAllItems();
+use __DIR__ Model\ItemManager; . '/../Model/ItemManager.php';
 
-require __DIR__ . '/../View/item.php';
+require __DIR__. '/../View/item.php';
+
+class ItemController
+{
+
+    public function index()
+    {
+
+        $itemManager = new ItemManager();
+        $items = $itemManager->selectAllItems();
+    }
+
+}
 ?>
